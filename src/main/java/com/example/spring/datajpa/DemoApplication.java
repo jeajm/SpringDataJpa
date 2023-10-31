d28b7a2ec81a9f42296606c6cce9b6c5da21cf3a
jeajm <111103928+jeajm@users.noreply.github.com>
2023-10-31 18:30:37 +0000

    change package name from withGit to withoutGit

--- src/main/java/com/example/spring/datajpa/DemoApplication.java
+++ src/main/java/com/example/spring/datajpa/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.spring.datajpa.Git;
+package com.example.spring.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
--- src/test/java/com/example/spring/datajpa/DemoApplicationTests.java
+++ src/test/java/com/example/spring/datajpa/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.spring.datajpa.Git;
+package com.example.spring.datajpa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
